6fef290970afbf7ba613ac3db284b3a2ba159e15
Lorenz Gonda <lorenzgonda@gmail.com>
2018-01-08 20:12:44 +0800

    Flatten Array

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,20 +1,21 @@
 
 public class Percolation {
 	
-	private boolean[][] grid;
+	private boolean[] grid;
 	private int nOpenSites = 0;
 	private int n;
 	
 	public Percolation(int n) {
-		grid = new boolean[n+1][n+1];
+		grid = new boolean[n*n];
 		this.n = n;
 	}
 	
 	public void open(int row, int col) {
 		if (!check(row) || !check(col))
 			throw new IllegalArgumentException();
-		if (!grid[row][col]) {
-			grid[row][col] = true;
+		int flat = flat(row, col);
+		if (!grid[flat]) {
+			grid[flat] = true;
 			nOpenSites++;
 		}
 	}
@@ -22,7 +23,7 @@ public class Percolation {
 	public boolean isOpen(int row, int col) {
 		if (!check(row) || !check(col))
 			throw new IllegalArgumentException();
-		return grid[row][col];
+		return grid[flat(row, col)];
 	}
 	
 	public boolean isFull (int row, int col) {
@@ -41,6 +42,10 @@ public class Percolation {
 	
 	private boolean check(int coord) {
 		return coord >= 1 && coord <= n;
+	}
+	
+	int flat(int row, int col) {
+		return n*(row-1) + (col-1);
 	}
 	
 }
